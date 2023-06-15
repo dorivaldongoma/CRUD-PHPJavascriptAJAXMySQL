963890d1ef6366e0ad92216984d1cb8a8c563007
dorivaldongoma <dorivaldongoma@gmail.com>
2023-06-15 21:07:19 +0100

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,12 @@
+# CRUD com PHP, MySQL, JavaScript e AJAX
+![Capa](img/capa.JPG)
+## Sobre o projecto
+[![Licença: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://github.com/dorivaldongoma/CRUD-PHPJavascriptAJAXMySQL/blob/main/LICENSE)
+
+CRUD usando base de dados MySQL com uma aplicação web desenvolvida em JavaScript e PHP, utilizando a técnica AJAX e a codificação JSON.
+
+Utilizei o PHP para reutilizar um modelo com cabeçalho e rodapé, além de conectá-lo ao banco de dados MySQL e responder às solicitações do JavaScript.
+
+Agora, com JavaScript, fiz todo o processamento do lado do cliente. Enquanto o PHP é responsável por renderizar as visualizações e lidar com as solicitações.
+
+Também usei o SweetAlert para os alertas, o fetch para realizar chamadas AJAX e o Bulma para os estilos.
---
+++ crud_php_js_ajax_mysql.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 15-Jun-2023 às 19:45
+-- Versão do servidor: 5.7.26
+-- versão do PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud_php_js_ajax_mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `descricao` varchar(1024) NOT NULL,
+  `preco` decimal(9,2) NOT NULL,
+  `cadastrado` datetime DEFAULT NULL,
+  `editado` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
